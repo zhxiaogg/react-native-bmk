4482128c1c2eb659a38fdf01115333c61d46b876
zhxiaog <zhxiaog@outlook.com>
2017-07-17 11:14:37 +0800

    export more map view properties

--- ios/react-native-bmk/react-native-bmk/BMKMapViewManager.m
+++ ios/react-native-bmk/react-native-bmk/BMKMapViewManager.m
@@ -7,6 +7,7 @@
 //
 
 #import "BMKMapViewManager.h"
+#import "RCTConvert+CoreLocation.h"
 
 #import <BaiduMapAPI_Map/BMKMapView.h>
 
@@ -14,9 +15,91 @@
 
 RCT_EXPORT_MODULE()
 
-- (UIView *)view
-{
+- (UIView *)view {
     return [[BMKMapView alloc] init];
 }
 
+// property delegate
+// property mapType
+RCT_EXPORT_VIEW_PROPERTY(mapType, BMKMapType)
+
+// property region
+// property limitMapRegion
+// property compassPosition
+// property compassSize
+// property centerCoordinate
+RCT_EXPORT_VIEW_PROPERTY(centerCoordinate, CLLocationCoordinate2D)
+// property zoomLevel
+RCT_EXPORT_VIEW_PROPERTY(zoomLevel, float)
+// property minZoomLevel
+// property maxZoomLevel
+// property rotation
+// property overlooking
+// property buildingsEnabled
+// property showMapPoi
+RCT_EXPORT_VIEW_PROPERTY(showMapPoi, BOOL)
+// property trafficEnabled
+// property baiduHeatMapEnabled
+// property gesturesEnabled
+// property zoomEnabled
+RCT_EXPORT_VIEW_PROPERTY(zoomEnabled, BOOL)
+// property zoomEnabledWithTap
+RCT_EXPORT_VIEW_PROPERTY(zoomEnabledWithTap, BOOL)
+// property scrollEnabled
+// property overlookEnabled
+// property rotateEnabled
+// property forceTouchEnabled
+// property showMapScaleBar
+RCT_EXPORT_VIEW_PROPERTY(showMapScaleBar, BOOL)
+// property mapScaleBarPosition
+// property mapScaleBarSize
+// property logoPosition
+RCT_EXPORT_VIEW_PROPERTY(logoPosition, BMKLogoPosition)
+// property visibleMapRect
+// property mapPadding
+// property updateTargetScreenPtWhenMapPaddingChanged
+// property ChangeWithTouchPointCenterEnabled
+@end
+
+
+@implementation RCTConvert (BMKMapType)
+
+RCT_ENUM_CONVERTER(BMKMapType,
+        (@{
+                @"BMKMapTypeNone": @(BMKMapTypeNone),
+                @"BMKMapTypeStandard": @(BMKMapTypeStandard),
+                @"BMKMapTypeSatellite": @(BMKMapTypeSatellite)
+        }),
+        BMKMapTypeStandard,
+        integerValue)
+
+@end
+
+@implementation RCTConvert (BMKLogoPosition)
+
+RCT_ENUM_CONVERTER(BMKLogoPosition,
+        (@{
+                @"BMKLogoPositionLeftBottom": @(BMKLogoPositionLeftBottom),
+                @"BMKLogoPositionLeftTop": @(BMKLogoPositionLeftTop),
+                @"BMKLogoPositionCenterBottom": @(BMKLogoPositionCenterBottom),
+                @"BMKLogoPositionCenterTop": @(BMKLogoPositionCenterTop),
+                @"BMKLogoPositionRightBottom": @(BMKLogoPositionRightBottom),
+                @"BMKLogoPositionRightTop": @(BMKLogoPositionRightTop)
+        }),
+        BMKLogoPositionLeftBottom,
+        integerValue)
+@end
+
+@implementation RCTConvert (CLLocationCoordinate2D)
+RCT_CONVERTER(CLLocationDegrees, CLLocationDegrees, doubleValue)
+
+RCT_CONVERTER(CLLocationDistance, CLLocationDistance, doubleValue)
+
++ (CLLocationCoordinate2D)CLLocationCoordinate2D:(id)json {
+    json = [self NSDictionary:json];
+    return (CLLocationCoordinate2D) {
+            [self CLLocationDegrees:json[@"latitude"]],
+            [self CLLocationDegrees:json[@"longitude"]]
+    };
+}
 @end
